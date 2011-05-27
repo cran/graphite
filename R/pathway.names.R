7968794e3ab3b987a426cd1dd5e86192d2de3c89
Gabriele Sales <gabriele.sales@unipd.it>
2011-05-27 00:00:00 +0000

    version 1.0

---
+++ R/pathway.names.R
@@ -0,0 +1,21 @@
+# Copyright 2011 Gabriele Sales <gabriele.sales@unipd.it>
+#
+#
+# This file is part of graphite.
+#
+# graphite is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License
+# version 3 as published by the Free Software Foundation.
+#
+# graphite is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public
+# License along with graphite. If not, see <http://www.gnu.org/licenses/>.
+
+
+pathway.names <- function(pathways) {
+  names(pathways$nodes)
+}
